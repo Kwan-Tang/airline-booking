f7eb3b1464fbc04f824548ce1894c3eadfd692a0
Kwan-Tang <41244977+Kwan-Tang@users.noreply.github.com>
2019-09-26 23:22:03 -0400

    fixed spacing

--- application.py
+++ application.py
@@ -13,5 +13,5 @@ def index():
     airport_origin = aliased(Airport)
     airport_destination = aliased(Airport)
     flights =  db.session.query(Flight.id,functions.concat(airport_origin.city,", ",airport_origin.country," (",airport_origin.airport_code,")").label("origin") \
-                                ,functions.concat(airport_destination.city,", ",airport_destination.country," (",airport_destination.airport_code,")").label("destination")                          ,Flight.duration).filter(Flight.origin_id==airport_origin.id).filter(Flight.destination_id==airport_destination.id).all()
+                                ,functions.concat(airport_destination.city,", ",airport_destination.country," (",airport_destination.airport_code,")").label("destination"),Flight.duration).filter(Flight.origin_id==airport_origin.id).filter(Flight.destination_id==airport_destination.id).all()
     return render_template("index.html",flights=flights)
